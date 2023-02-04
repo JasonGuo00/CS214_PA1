4a3b2fa5075e9894c113f544e5531b57c4953e7e
armenian-man <armen@karakashian.com>
2023-02-03 20:30:14 -0500

    added my files :)

---
+++ mymalloc.h
@@ -0,0 +1,17 @@
+#ifndef MYMALLOC_H
+#define MYMALLOC_H
+
+	#define MAX_BYTES 4096
+	
+	enum bool{false,true};
+	
+	typedef enum bool bool;
+	typedef char byte;
+	
+	void* mymalloc(size_t s, char* file, int line);
+	void myfree(void* ptr, char* file, int line);
+	
+	#define malloc(s) mymalloc(s, __FILE__, __LINE__)
+	#define free(ptr) myfree(ptr, __FILE__, __LINE__)
+	
+#endif
