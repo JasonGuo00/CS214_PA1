4529c9679eeb0c8921e7572e6cb04f9b885b0b7b
JasonGuo00 <jasonglobguo@gmail.com>
2023-02-06 17:48:56 -0500

    Updated Malloc, Fixed / Added Test Cases
    - Malloc properly takes 4 byte integers for the size meta tag
    
    - Fixed randomization in memgrind
    
    - Finished implementing task 3
    
    - Added task 5: allocates 120 randomly sized chunks, prints out 10 randomly chosen chunks and then frees them.

--- memgrind.c
+++ memgrind.c
@@ -24,6 +24,9 @@ int main(int argc, char* argv[argc+1]){
 
 	int runs = 120;
 
+	time_t t;
+	srand((unsigned) time(&t));
+
 	//Task 1//
 		set_stopwatch();
 
@@ -55,19 +58,34 @@ int main(int argc, char* argv[argc+1]){
 
 	//Task 3//
 		set_stopwatch();
+		int num_mallocs = 0;
+		int open_space = 0;
 
 		void* ptr_array2[runs];
-		for (int i = 0; i < runs; i++){
-			if (rand()%2==0){
-				ptr_array2[i] = malloc(s);
+
+		// This implementation frees the most recently allocated space, so basically like a stack
+		while(num_mallocs < 120) {
+			if(rand() % 2 == 0 && open_space < 120) {
+				ptr_array2[open_space] = malloc(s);
+				open_space++;
+				num_mallocs++;
 			}
-			else{
-				
+			else {
+				if(open_space > 0) {
+					free(ptr_array2[open_space - 1]);
+					open_space--;
+				}
+				// Otherwise, nothing to free, so do nothing this iteration.
 			}
 		}
 
+		// Free all allocated objects after malloc runs 120 times
+		for(int i = 0; i < open_space; i++) {
+			free(ptr_array2[i]);
+		}
+
 		set_stopwatch();
-		printf("Task 4 Time: %lf seconds\n", t1-t0);
+		printf("Task 3 Time: %lf seconds\n", t1-t0);
 	//--//
 
 	//Task 4//
@@ -87,6 +105,33 @@ int main(int argc, char* argv[argc+1]){
 
 		set_stopwatch();
 		printf("Task 4 Time: %lf seconds\n", t1-t0);
+	//--//
+
+	//Task 5//
+		set_stopwatch();
+
+		void* ptr_array3[runs];
+
+		int index = 0;
+		// run 120 times, allocating random numbers of bytes and freeing if there's no room left.
+		for(int i = 0; i < runs; i++) {
+			ptr_array3[index] = malloc((rand() % (((MAX_BYTES-5)/120)) - 1) + 1);
+			index++;
+		}
+
+		for(int i = 0; i < 10; i++) {
+			byte* chunk = ptr_array3[rand() % runs];
+			chunk -= 5;
+			printf("Random Chunk %d has size: %d\n", i, *((unsigned int*)chunk));
+		}
+
+		for(int i = 0; i < index; i++) {
+			if(ptr_array3[i])
+			free(ptr_array3[i]);
+		}
+
+		set_stopwatch();
+		printf("Task 5 Time: %lf seconds\n", t1-t0);
 	//--//
 
 	return EXIT_SUCCESS;
--- mymalloc.c
+++ mymalloc.c
@@ -4,6 +4,8 @@
 
 static byte memory[MAX_BYTES];
 
+// to retrive the "size" of the memory chunk:  *((unsigned int*) &memory[i])
+
 void* mymalloc(size_t size, char* file, int line){
 	if (size == 0){
 		return (void*) 0;
@@ -12,20 +14,20 @@ void* mymalloc(size_t size, char* file, int line){
 	void* payload_ptr = 0;
 		
 	//memory[i] is start of chunk header
-	//memory[i] = size, memory[i+1] = true or false if allocated
+	//memory[i -> i+1] = size, memory[i+2] = true or false if allocated
 	int seq_space_found = 0;
 	int i;
-	for (i = 0; i+size+2 < MAX_BYTES; i += memory[i]+2){
-		int chunk_size = memory[i];
-		char is_allocated = memory[i+1];
+	for (i = 0; i+size+5 < MAX_BYTES; i += *((unsigned int*) &memory[i]) + 5){
+		int chunk_size = *((unsigned int*) &memory[i]);
+		char is_allocated = memory[i+4];
 		if (is_allocated == 0){
 			if (chunk_size == 0){
 				//This chunk has never been allocated before,
 				//meaning no bytes ahead of it have been allocated either, so we
 				//don't need to worry about overwriting
-				memory[i] = size;
-				memory[i+1] = 1;
-				payload_ptr = &(memory[i+2]);
+				*((unsigned int*) &memory[i]) = size;
+				memory[i+4] = 1;
+				payload_ptr = &(memory[i+5]);
 				//finished allocating, stop searching
 				break;
 			}
@@ -33,30 +35,30 @@ void* mymalloc(size_t size, char* file, int line){
 				int bytes_left = seq_space_found+chunk_size-size; //size of all previous open chunks - size of payload we want to allocate
 				if (bytes_left < 0){
 					//chunk isn't big enough, keep searching
-					seq_space_found += chunk_size+2;
+					seq_space_found += chunk_size+5;
 				}
 				else {
 					//found enough unallocated space, allocate chunk(s)
 					int new_header_index = i - seq_space_found;
-					memory[new_header_index] = size;
-					memory[new_header_index+1] = 1;
-					payload_ptr = &(memory[new_header_index+2]);
+					*((unsigned int*)&memory[new_header_index]) = size;
+					memory[new_header_index+4] = 1;
+					payload_ptr = &(memory[new_header_index+5]);
 					
-					//minimum size of a chunk is 3 (one byte for size, one byte for isAllocated,
+					//minimum size of a chunk is 6 (four bytes for size, one byte for isAllocated,
 					//and one byte for payload)
-					if (bytes_left >= 3){
+					if (bytes_left >= 6){
 						//there is room for another chunk, so split into two
-						int split_header_index = new_header_index+2+size;
-						memory[split_header_index] = bytes_left;
-						memory[split_header_index+1] = 0;
+						int split_header_index = new_header_index+5+size;
+						*((unsigned int*) &memory[split_header_index]) = bytes_left;
+						memory[split_header_index+4] = 0;
 					}
 					else{
 						//there are a couple extra bytes in this chunk but not enough to 
 						//split the chunk into two. include these spare bytes in the chunk 
 						//we just allocated
-						memory[new_header_index] += bytes_left;
+						*((unsigned int*) &memory[new_header_index]) += bytes_left;
 					}
-					
+
 					//finished allocating, stop searching
 					break;
 				}
@@ -90,12 +92,12 @@ void myfree(void* ptr, char* file, int line){
 	bool isPayloadHeader = false;
 	
 	int i;
-	for (i = 0; i < MAX_BYTES; i += memory[i]+2){
-		if (&(memory[i+2]) == ptr){
+	for (i = 0; i < MAX_BYTES; i += *((unsigned int*) &memory[i])+5){
+		if (&(memory[i+5]) == ptr){
 			isPayloadHeader = true;
 			break;
 		}
-		if (memory[i]+2 == 0){
+		if (*((unsigned int*) &memory[i])+5 == 0){
 			//not supposed to happen, but if it somehow does, throw the "pointer
 			//does not point to payload header error" to prevent infinite loop
 			break;
@@ -106,7 +108,7 @@ void myfree(void* ptr, char* file, int line){
 		return;
 	}
 	
-	if (memory[i+1] == 0){
+	if (memory[i+4] == 0){
 		//Was already freed or never allocated
 		printf("Error (%s line %d): Attempt to free freed/unallocated memory address\n",  file, line);
 		return;
@@ -115,5 +117,5 @@ void myfree(void* ptr, char* file, int line){
 	//Errors handled//
 	
 	//set chunk allocation byte to freed/unallocated
-	memory[i+1] = 0;
+	memory[i+4] = 0;
 }
